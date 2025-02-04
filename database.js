e7fb2d1358199e08e3ba4e45ffa55e753605dcff
Likhitha-Setty <likhithasetty5@gmail.com>
2025-02-04 14:30:17 +0530

    updated database.js

--- database.js
+++ database.js
@@ -1,6 +1,4 @@
 const { Pool } = require("pg");
-
-// Configure the database connection pool
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
@@ -9,15 +7,12 @@ const pool = new Pool({
   port: 5432,
 });
 
-// Log successful connection
 pool.on("connect", () => {
   console.log("PostgreSQL connected");
 });
-
-// Log errors
 pool.on("error", (err) => {
   console.error("Unexpected error on PostgreSQL client", err);
-  process.exit(-1); // Exit the process in case of critical errors
+  process.exit(-1); 
 });
 
 module.exports = pool;
